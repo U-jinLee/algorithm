ffe649e4ccb2f5803b8e70bf29e8167c442ebce3
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-08-28 00:02:20 +0900

    기능추가: BFS 공부에 필요한 그래프와 배열 생성

--- algorithm_m/src/BFS/bfs_basic.java
+++ algorithm_m/src/BFS/bfs_basic.java
@@ -1,4 +1,13 @@
 package BFS;
 
-public class bfs_basic {	
+public class bfs_basic {
+	//임의의 가상 그래프 생성
+	/*0~8번까지 노드이며 각 노드가 어떤 노드와 연결돼 있는지를 의미.*/
+					/*0번은 제외*/
+	public static void main(String[] args) {
+		int graph[][] = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+		/*방문 처리를 하기 위한 boolean 배열*/
+		boolean[] visited = new boolean[9];		
+	}
+	
 }
