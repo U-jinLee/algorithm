a010e2ed9341d823086549db9adb4735a94c6a45
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-08-30 22:34:49 +0900

    문서: DFS 기본 문서 생성

--- algorithm_m/src/BFS/dfs_basic.java
+++
@@ -1,4 +0,0 @@
-package BFS;
-
-public class dfs_basic {
-}
---
+++ algorithm_m/src/DFS/dfs_basic.java
@@ -0,0 +1,11 @@
+package DFS;
+
+import java.util.Scanner;
+
+public class dfs_basic {
+	Scanner sc = new Scanner(System.in);
+	
+	int node = sc.nextInt();/*정점의 갯수*/
+	int line = sc.nextInt();/*간선의 갯수*/
+	int startNum = sc.nextInt();/*탐색 시작 정점 번호*/
+}
