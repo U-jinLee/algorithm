37fe4a47633685880e7ff0a245255c8709295ce3
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-09-02 00:57:59 +0900

    기능추가: 미로찾기 함수 기능 구현
    미로찾기에 필요한 함수를 recursion에 맞게 구현하고 주석을 통한 설명
    
    길 찾기: #24
    미로 상태 호출: #45

--- algorithm_m/src/recursion/Recursion_maze.java
+++ algorithm_m/src/recursion/Recursion_maze.java
@@ -9,28 +9,53 @@ public class Recursion_maze {
 	/*미로 생성*/
 	private static int N = 8;
 	private static int[][] maze = {
-			{0,0,0,0,0,0,0,1},
-			{0,1,1,0,1,1,0,1},
-			{0,0,0,1,0,0,0,1},
-			{0,1,0,0,1,1,0,0},
-			{0,1,1,1,0,0,1,1},
-			{0,1,0,0,0,1,0,1},
-			{0,0,0,1,0,0,0,1},
-			{0,1,1,1,0,1,0,0},
+		      {0, 0, 0, 0, 0, 0, 0, 1},
+		      {0, 1, 1, 0, 1, 1, 0, 1},
+		      {0, 0, 0, 1, 0, 0, 0, 1},
+		      {0, 1, 0, 0, 1, 1, 0, 0},
+		      {0, 1, 1, 1, 0, 0, 1, 1},
+		      {0, 1, 0, 0, 0, 1, 0, 1},
+		      {0, 0, 0, 1, 0, 0, 0, 1},
+		      {0, 1, 1, 1, 0, 1, 0, 0}
 	};
 	
 	
 	/*미로의 길을 찾을 함수 생성*/
 	public static boolean findMazePath(int x, int y) {
-		return true;
+		/*두 좌표가 -의 값을 가지거나 지정된 범위를 초과했을 시*/
+		if(x < 0 || y < 0 || x>= N || y >= N) {
+			return false;
+		} else if(maze[x][y] != PATHWAY_COLOR) { /*'지나갈 수 없는 길'이거나 '출구가 없는 길'일 때*/
+			return false;
+		} else if (x == N-1 && y == N-1) { /*출구에 도착했을 때*/
+			maze[x][y] = PATH_COLOR;
+			return true;
+		} else {
+			maze[x][y] = PATH_COLOR; // 지나온 길은 path_color 처리
+			if(findMazePath(x-1, y) || findMazePath(x, y+1) || 
+				findMazePath(x+1, y) || findMazePath(x, y-1)) {
+				return true;
+			}
+			maze[x][y] = BLOCKED_COLOR;
+			return false;
+		}
 	}
 	
 	/*미로를 출력할 함수 생성*/
 	public static void printMaze() {
-		
+		for(int i = 0 ; i < N; i++) {
+			System.out.print("{");
+			for(int j = 0; j < N; j++) {
+				System.out.print(maze[i][j]+" ");
+			}
+			System.out.println("}");
+		}
+		System.out.println();
 	}	
 	
 	public static void main(String[] args) {
-		
+		printMaze();
+		findMazePath(0, 0);
+		printMaze();
 	}
 }
