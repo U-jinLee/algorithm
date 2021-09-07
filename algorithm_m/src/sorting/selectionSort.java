2a352248c8bd8bbeadc90a565cde30040d307c03
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-09-07 23:37:52 +0900

    문서: 선택정렬 문서 생성

--- algorithm_m/src/sorting/selectionSort.java
+++ algorithm_m/src/sorting/selectionSort.java
@@ -3,8 +3,16 @@ package sorting;
 public class selectionSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	static void selectionSorts(int[] argArray, int num) {
+		for(int i=0; i < num -1; i++) {
+			int min = i;
+			
+		}
+		
+	
+	}
+	
 }
