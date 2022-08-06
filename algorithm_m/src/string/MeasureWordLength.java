16e243c2477dc75f553e67fb186409bd6cd4c10d
Yoojin Lee <yoojinLee.dev@gmail.com>
2022-08-06 23:00:04 +0900

    Feat: Add MeasureWordLength.java

---
+++ algorithm_m/src/string/MeasureWordLength.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Scanner;
+
+public class MeasureWordLength {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = sc.nextLine();
+        System.out.println(result.length());
+    }
+}
