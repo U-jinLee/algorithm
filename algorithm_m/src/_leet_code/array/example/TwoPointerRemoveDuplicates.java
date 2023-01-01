9a37f3d4666d39bd5b5eb5fc6b373e26b0577576
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-01-02 00:56:38 +0900

    doc: Add TwoPointerRemoveDuplicates explain

--- algorithm_m/src/_leet_code/array/example/TwoPointerRemoveDuplicates.java
+++ algorithm_m/src/_leet_code/array/example/TwoPointerRemoveDuplicates.java
@@ -10,8 +10,9 @@ public class TwoPointerRemoveDuplicates {
         if(nums == null) return 0;
 
         int writerPointer = 1;
-
+        // 순차 배열을 통해서 진행 writerPointer와 readPointer의 순서를 맞추는 것이 중요
         for (int readPointer = 1; readPointer < nums.length; readPointer++) {
+            //조건부 검색(앞 뒤 값이 다를 시 현재 위치에 값을 넣고 write 포인트 뒤로 당기기)
             if(nums[readPointer] != nums[readPointer - 1]) {
                 nums[writerPointer] = nums[readPointer];
                 writerPointer++;
