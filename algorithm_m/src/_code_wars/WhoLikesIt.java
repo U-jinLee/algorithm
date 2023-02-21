99b890b0e9c20daece0933ec9b5b6a92e60946e1
yoojin Lee <yj.lee@tenth.kr>
2023-02-21 16:42:10 +0900

    Feat: Add refine problems

--- algorithm_m/src/_code_wars/WhoLikesIt.java
+++ algorithm_m/src/_code_wars/WhoLikesIt.java
@@ -2,10 +2,17 @@ package _code_wars;
 
 public class WhoLikesIt {
     public static void main(String[] args) {
+        System.out.println(whoLikesIt());
         System.out.println(whoLikesIt("Peter"));
         System.out.println(whoLikesIt("Jacob", "Alex"));
         System.out.println(whoLikesIt("Max", "John", "Mark"));
         System.out.println(whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        //add refine method
+        System.out.println(refinedWhoLikesIt());
+        System.out.println(refinedWhoLikesIt("Peter"));
+        System.out.println(refinedWhoLikesIt("Jacob", "Alex"));
+        System.out.println(refinedWhoLikesIt("Max", "John", "Mark"));
+        System.out.println(refinedWhoLikesIt("Alex", "Jacob", "Mark", "Max"));
     }
     public static String whoLikesIt(String... names) {
         if(names.length == 0) {
@@ -18,6 +25,16 @@ public class WhoLikesIt {
             return names[0] + ", " + names[1] + " and " + names[2] + " like this";
         } else {
             return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        }
+    }
+
+    public static String refinedWhoLikesIt(String... names) {
+        switch (names.length) {
+            case 0: return "no one likes this";
+            case 1: return String.format("%s likes this", names[0]);
+            case 2: return String.format("%s and %s like this", names[0], names[1]);
+            case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
         }
     }
 }
