304992346be89b5fa484c289bf275c2d8264aecf
yoojin Lee <yj.lee@tenth.kr>
2023-02-28 12:28:12 +0900

    feat: Add bit counting

---
+++ algorithm_m/src/_code_wars/BitCounting.java
@@ -0,0 +1,20 @@
+package _code_wars;
+
+public class BitCounting {
+    public static void main(String[] args) {
+        System.out.println(countBits(1234));
+        System.out.println(countBits(4));
+        System.out.println(countBits(7));
+        System.out.println(countBits(9));
+        System.out.println(countBits(10));
+        System.out.println(parseCountBits(1234));
+        System.out.println(parseCountBits(4));
+        System.out.println(parseCountBits(7));
+        System.out.println(parseCountBits(9));
+        System.out.println(parseCountBits(10));
+    }
+    public static int countBits(int n) {
+        return Integer.toBinaryString(n).replace("0", "").length();
+    }
+
+}
