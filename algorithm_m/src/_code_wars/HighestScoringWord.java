9f0a23a6fa52db59b52bce4aad4d9270ee70f5bd
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-04-11 12:15:42 +0900

    feat: Add refine highestScoringWord

--- algorithm_m/src/_code_wars/HighestScoringWord.java
+++ algorithm_m/src/_code_wars/HighestScoringWord.java
@@ -33,6 +33,13 @@ public class HighestScoringWord {
         return result;
     }
 
+    private static long refineGetWordScore(String word) {
+        int result = 0;
+        char[] chars = word.toCharArray();
+        for (char c : chars) result += c + 'a' - 1;
+        return result;
+    }
+
     public enum Alphabet {
         a(1), b(2), c(3), d(4), e(5), f(6), g(7), h(8), i(9), j(10), k(11), l(12), m(13), n(14), o(15), p(16), q(17), r(18), s(19), t(20), u(21), v(22), w(23), x(24), y(25), z(26);
         public int num;
