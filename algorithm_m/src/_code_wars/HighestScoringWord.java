6a638a940a8e63e0c55b9b1dda38e3f0f9c6ceb6
DESKTOP-D8U0NPS\USER-PC <leeujin1029@naver.com>
2023-04-12 23:10:39 +0900

    feat: Add refine result

--- algorithm_m/src/_code_wars/HighestScoringWord.java
+++ algorithm_m/src/_code_wars/HighestScoringWord.java
@@ -8,6 +8,10 @@ public class HighestScoringWord {
         System.out.println(high(test));
         System.out.println(high(test2));
         System.out.println(high(test3));
+        System.out.println();
+        System.out.println(refineHigh(test));
+        System.out.println(refineHigh(test2));
+        System.out.println(refineHigh(test3));
     }
     public static String high(String s) {
         long highestScore = 0;
@@ -33,13 +37,6 @@ public class HighestScoringWord {
         return result;
     }
 
-    private static long refineGetWordScore(String word) {
-        int result = 0;
-        char[] chars = word.toCharArray();
-        for (char c : chars) result += c + 'a' - 1;
-        return result;
-    }
-
     public enum Alphabet {
         a(1), b(2), c(3), d(4), e(5), f(6), g(7), h(8), i(9), j(10), k(11), l(12), m(13), n(14), o(15), p(16), q(17), r(18), s(19), t(20), u(21), v(22), w(23), x(24), y(25), z(26);
         public int num;
@@ -50,6 +47,30 @@ public class HighestScoringWord {
         public int getInt() {
             return this.num;
         }
+    }
+
+    public static String refineHigh(String s) {
+        long highestScore = 0;
+        int highestNumber = 0;
+
+        // 문장을 단어로 분할
+        String[] words = s.split(" ");
+        // 단어를 캐릭터로 변환 후 점수를 계산
+        for(int i = 0; i < words.length; i++) {
+            if(getWordScore(words[i]) > highestScore) {
+                highestScore = refineGetWordScore(words[i]);
+                highestNumber = i;
+            }
+        }
+
+        return words[highestNumber];
+    }
+
+    private static long refineGetWordScore(String word) {
+        int result = 0;
+        char[] chars = word.toCharArray();
+        for (char c : chars) result += c - 'a' + 1;
+        return result;
     }
 
 }
